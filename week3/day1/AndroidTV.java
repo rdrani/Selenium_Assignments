48de34ae73bdd2ed3a3fbbf1a8fca384a9e9affc
rdrani <132195558+rdrani@users.noreply.github.com>
2023-05-13 10:40:43 -0400

    Delete week3 directory

--- week3/day1/AndroidTV.java
+++
@@ -1,8 +0,0 @@
-package assignments.week3.day1;
-
-public abstract class AndroidTV implements Android{
-	@Override
-	public void openApp() {
-		System.out.println("Opening app -> Inside Android TV");
-	}
-}
