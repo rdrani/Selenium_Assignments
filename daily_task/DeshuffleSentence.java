01256905cbd65defa4c229ea0b07149ff97e435b
Rani R <132195558+rdrani@users.noreply.github.com>
2023-05-29 10:53:53 -0400

    Create DeshuffleSentence.java
    Java Problem (14/20)
    
    A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
    
    A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
    
    For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
    Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
    
    Example 1:
    
    Input: s = "is2 sentence4 This1 a3"
    Output: "This is a sentence"
    Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
    Example 2:
    
    Input: s = "Myself2 Me1 I4 and3"
    Output: "Me Myself and I"
    Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.

---
+++ daily_task/DeshuffleSentence.java
@@ -0,0 +1,18 @@
+package assignments.daily_task;
+
+public class DeshuffleSentence {
+    public static void main(String args[]) {
+        String s = "is2 sentence4 This1 a3";
+//        String s = "Myself2 Me1 I4 and3";
+        String[] splitArr = s.split(" ");
+        String[] orderedArr = new String[splitArr.length];
+        int index = 0;
+        for(String entry:splitArr){
+            index = Character.getNumericValue(entry.charAt(entry.length()-1));
+            orderedArr[index-1] = entry.substring(0,entry.length()-1);
+        }
+        for (String word : orderedArr) {
+        	System.out.print(word + " ");
+		    }
+    }
+}
