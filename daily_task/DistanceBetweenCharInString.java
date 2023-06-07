94ed69b111c5ea7087593f6a192e22452b865401
Rani R <132195558+rdrani@users.noreply.github.com>
2023-06-07 14:13:08 -0400

    Update DistanceBetweenCharInString.java
    Changes made to account for closest occurrence of the given character from left and right of each index position

--- daily_task/DistanceBetweenCharInString.java
+++ daily_task/DistanceBetweenCharInString.java
@@ -3,13 +3,17 @@ package assignments.daily_task;
 public class DistanceBetweenCharInString {
     public static void main(String args[]) {
         //String s = "aaab"; char c = 'b';
-        String s = "dbcadcbaadacccbda"; char c = 'a';
+        String s = "dbcadcbaadacccbd"; char c = 'a';
         int[] arr = new int[s.length()];
         int count = s.indexOf(c);
+        int l = 0, r = 0;
         for(int i = 0; i < s.length(); i++){
+            r = s.indexOf(c,i) - i;
             if(s.charAt(i) == c)
                 count = i;
-            arr[i] = Math.abs(i - count);
+            l = Math.abs(i - count);
+            //System.out.println("i:"+ i+" l:"+l+" r:"+r);
+            arr[i] = (r > 0)&&(l > r)? r:l;
         }
         for(int i: arr)
             System.out.print(i+" ");
